acea4855b9e3c9a7f675c8a34a21114574da5126
asmaa7md <asmaa7md98@gami.com>
2023-10-17 10:23:34 -0700

    2

--- string_fields.c
+++ string_fields.c
@@ -8,23 +8,24 @@
  *
  * Return: new pointer
  */
+
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+	int s = 0;
 
 	if (*p != '.')
 		return (p);
 	p++;
 	if (*p == '*')
 	{
-		d = va_arg(ap, int);
+		s = va_arg(ap, int);
 		p++;
 	}
 	else
 	{
 		while (_isdigit(*p))
-			d = d * 10 + (*p++ - '0');
+			s = s * 10 + (*p++ - '0');
 	}
-	params->precision = d;
+	params->precision = s;
 	return (p);
 }
