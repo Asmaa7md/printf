b343321a53f8936ecf5c798bb32b09f5fede2f10
asmaa7md <asmaa7md98@gami.com>
2023-10-17 05:27:29 -0700

    2

--- size.c
+++ size.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
+ * _sring - prints a string with newline
+ * @str:  string to print
  *
  * Return: void
- */
-int _puts(char *str)
+*/
+
+int _string(char *str)
 {
 	char *a = str;
 
@@ -21,18 +22,20 @@ int _puts(char *str)
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
+*/
+
 int _putchar(int c)
 {
-	static int i;
+	static int r;
 	static char buf[OUTPUT_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	if (c == BUF_FLUSH || r >= OUTPUT_BUF_SIZE)
 	{
-		write(1, buf, i);
-		i = 0;
+		write(1, buf, r);
+		r = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[r++] = c;
 	return (1);
+
 }
