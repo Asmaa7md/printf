f1d28df41aa50bef0e30f1d463c307d584057eeb
Asmaa7md <asmaa7md98@gmail.com>
2023-10-17 05:01:58 -0700

    x

--- _printf.c
+++ _printf.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
+ * _printf - produces output according to a format
+ * @format: format string
  *
- * Return: number of bytes printed
- */
+ * Return: number of characters printed
+*/
+
 int _printf(const char *format, ...)
 {
-	int sum = 0;
+	int s = 0;
 	va_list ap;
 	char *p, *start;
 	params_t params = PARAMS_INIT;
@@ -17,14 +18,16 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
+
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
+
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
 		if (*p != '%')
 		{
-			sum += _putchar(*p);
+			s += _putchar(*p);
 			continue;
 		}
 		start = p;
@@ -37,13 +40,16 @@ int _printf(const char *format, ...)
 		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
+
 		if (!get_specifier(p))
-			sum += print_from_to(start, p,
+			s += print_from_to(start, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			sum += get_print_func(p, ap, &params);
+			s += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-	return (sum);
+
+	return (s);
+
 }
